f16955535411dd545dc3bd62dcd55f9cd71f1ddf
Liang Chen <161270933+lche0235@users.noreply.github.com>
2024-12-17 17:36:29 +1100

    Add payout calculation functionality to the Payout page, including event listener for calculate button and logic to determine winners and losers based on net amounts.

--- payoutPage/payoutPage.js
+++ payoutPage/payoutPage.js
@@ -1,6 +1,7 @@
 // add player button
 const playersTableBody = document.getElementById('players_table');
 const addPlayersBtn = document.querySelector(".button_add");
+const calculateBtn = document.querySelector(".button_calculate");
 
 function createNewRow() {
     const newRow = document.createElement('tr');
@@ -53,3 +54,29 @@ addPlayersBtn.addEventListener("click", () => {
 });
 
 playersTableBody.appendChild(createNewRow());
+
+// payout calculation
+function calculatePayouts() {
+    const rows = playersTableBody.querySelectorAll('tr');
+
+    const losers = []; 
+    const winners = [];
+
+    // if net > 0, winners.push, is net < 0 losers.push
+    rows.forEach(row => {
+        const name = row.querySelector('input[placeholder="Enter name"]').value;
+        const net = Number(row.querySelector(".profit_display").textContent);
+
+        if (net < 0) {
+            losers.push({name, amount: Math.abs(net)});
+        } else if (net > 0) {
+            winners.push({name, amount: Math.abs(net)});
+        }
+    })
+    
+    // sort the arrays by size
+    winners.sort((a,b) => a.amount > b.amount ? 1:-1);
+    losers.sort((a,b) => a.amount > b.amount ? 1:-1);
+}
+
+calculateBtn.addEventListener("click", calculatePayouts);
