3acfad8315e9dc4c0ced1ac10a3928400c8b4d6d
Liang Chen <161270933+lche0235@users.noreply.github.com>
2024-12-19 01:57:36 +1100

    Implement game data loading from Firestore with stats: name, gamesPlayed, and totalNet

--- leaderboardPage/leaderboardPage.js
+++ leaderboardPage/leaderboardPage.js
@@ -1,7 +1,41 @@
 const leaderboardTable = document.getElementById('leaderboard_table');
 
+// format currency to have the '-' sign outside of the $
 function formatCurrency(amount) {
     const formatted = amount < 0 ? `-$${Math.abs.amount}` : `$${amount}`;
     const className = amount < 0 ? 'loss' : amount > 0 ? 'profit' : 'neutral';
    return `<span class="${className}">${formatted}</span>`;
 }
+
+// loading games from backend
+db.collection('games')
+    .get()
+    
+    .then(snapshot => {
+        const playerStats = {}
+        snapshot.forEach(doc => {
+            const game = doc.data();
+            game.players.forEach(player => {
+                const name = player.name.toUpperCase();
+
+                // intializes player with their stats if new player
+                if (!playerStats[name]) {
+                    playerStats[name] = {
+                        name: name,
+                        gamesPlayed: 0,
+                        totalNet: 0,
+                    };
+                }
+
+                // updates these every time we see the player in game history
+               playerStats[name].gamesPlayed++;
+               playerStats[name].totalNet += player.net;
+            })
+        });
+        // test
+       console.log('Player Stats:', playerStats);
+    })
+    .catch(error => {  
+        console.error("Error loading games:", error); 
+        alert("Error loading leaderboard data");      
+    });
